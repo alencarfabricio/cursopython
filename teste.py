1de73aecb2067bf92ef30c0c60b12da2a5700442
alencar <fabricio.alencar@armazemparaiba.com.br>
2015-07-02 13:02:32 -0300

    loop em uma linha

---
+++ teste.py
@@ -0,0 +1,13 @@
+"""
+Imprimir os numeros pares de 0 a 100.
+"""
+
+#-*- coding: utf-8 -*- 
+
+instrumentos = ['bateria','violao','teclado','contra1']
+
+for instrumento in instrumentos:
+    print(instrumento)
+
+print (", ".join(i for i in instrumentos))
+    
