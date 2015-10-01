35ffbd71f5f1181988b8f2a30f4ee1dcccb4603a
alencarfabricio <fabricio.alencar@armazemparaiba.com.br>
2015-10-01 14:10:24 -0300

    Ajuste Projeto Final

--- Projetos/vassistencia/assistencia/cliente/admin.py
+++ Projetos/vassistencia/assistencia/cliente/admin.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from .models import Cliente
+from ..ordem.models import Ordem
 
 # Register your models here.
 
 #@admin.register(Contato)
 class ClienteAdmin(admin.ModelAdmin):
     #Colunas que vai ser aparecer no site
-    list_display = ('nome', 'email', 'telefone','sexo','cidade')
+    list_display = ('nome', 'email', 'telefone','sexo','cidade', 'ordem',)
     # lupa de pesquisa
     search_fields = ('nome', 'email','sexo')
 
--- Projetos/vassistencia/assistencia/cliente/models.py
+++ Projetos/vassistencia/assistencia/cliente/models.py
@@ -1,5 +1,6 @@
 # models eh o modulo
 from django.db import models
+from django.utils.safestring import mark_safe
 
 # Create your models here.
 # Criando a classe Contato que herda da classe Model do modulo models
@@ -21,3 +22,11 @@ class Cliente(models.Model):
     def __str__(self):
         return self.nome
 
+
+    def ordem(self):
+        html = ''
+        for ordem in self.ordem_set.exclude(situacao='1'):
+            html += '<p><button onclick="window.open(\'/admin/ordem/ordem/{0}/?_popup=1\',\'OS\',\'status=1\')" target>O.S: {1}</button></p>'.format(ordem.id, ordem.id)
+        return mark_safe(html)
+    ordem.short_description = 'Ordem de serviços em aberto'
+    ordem = property(ordem) #nova propriedade para reconhecer no list_display do admin
